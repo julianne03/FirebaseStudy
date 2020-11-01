f891f29cce7dc4738a85fee4d045cda7cf963711
Seungmin Yi <s2019w33@e-mirim.hs.kr>
2020-11-01 15:44:32 +0900

    cloudstorage 파일 추가 (3개)

---
+++ app/src/main/java/kr/hs/emirim/seungmin/firebasestart/cloudstorage/CloudStorageActivity.java
@@ -0,0 +1,16 @@
+package kr.hs.emirim.seungmin.firebasestart.cloudstorage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import kr.hs.emirim.seungmin.firebasestart.R;
+
+public class CloudStorageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cloud_storage);
+    }
+}
